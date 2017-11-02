4dfffc7cc6f2c31968a4929ef1636cc6e07eddad
John Chleborad <jchleborad@gmail.com>
2017-11-01 20:55:16 -0700

    ComplexDataModel

---
+++ ContosoUniversity/Migrations/20171102021544_ColumnFirstName.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ContosoUniversity.Migrations
+{
+    public partial class ColumnFirstName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FirstMidName",
+                table: "Student",
+                newName: "FistName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FistName",
+                table: "Student",
+                newName: "FirstMidName");
+        }
+    }
+}
--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -1,16 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
     public class Student
     {
         public int ID { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        [Column("FistName")]
+        [Display(Name = "First Name")]
         public string FirstMidName { get; set; }
+        [DataType(DataType.Date)]
+
+        //ApplyFormatInEditMode is for the text box during editing
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
-        
-        //Add Email Address to Project
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }
+
+        //Add Email Address w/validation to Project
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email Address")]
         public string EmailAddress { get; set; }
 
         public ICollection<Enrollment> Enrollments { get; set; } //holds all enrollments related to the student (one to many [student to enrollment])
