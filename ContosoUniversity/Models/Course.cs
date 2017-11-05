4501692b9dbb9277ce12717fb07da6d6d559e388
John Chleborad <jchleborad@gmail.com>
2017-11-05 16:20:09 -0700

    Creating, Reading & Updating data

--- ContosoUniversity/Models/Course.cs
+++ ContosoUniversity/Models/Course.cs
@@ -7,14 +7,14 @@ namespace ContosoUniversity.Models
     public class Course
     {
         [DatabaseGenerated(DatabaseGeneratedOption.None)] //Allows for direct entry of primary key rather than having database generate it
-        [Display(Name = "Number")]
+        [Display(Name = "Course No.")]
         public int CourseID { get; set; }
 
         [StringLength(50, MinimumLength = 3)]
         public string Title { get; set; }
 
         [Range(0, 5)]
-        public int Credits { get; set; }
+        public int Credits { get; set; }    
 
         public int DepartmentID { get; set; }
 
